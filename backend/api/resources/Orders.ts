fef5d689f05f808b082c6eed5a10770d0d7bd728
justlogicalahmad <rabiu2993@gmail.com>
2024-06-19 09:51:45 -0700

    Created the list of orders storage

---
+++ backend/api/resources/Orders.ts
@@ -0,0 +1,32 @@
+export const orders = [
+  {
+    title: "Bread and Beans",
+    price: 98,
+    imageUrl: "https://allnigerianfoods.com/wp-content/uploads/Ewa-agoyin.jpg",
+    itemCount: 4,
+  },
+  {
+    title: "Rice and Chicken",
+    price: 98,
+    imageUrl:
+      "https://cdn.filestackcontent.com/resize=width:430,height:430,fit:crop,align:center/5ngAPL80SuCvpsEb7S2f",
+    itemCount: 4,
+  },
+  {
+    title: "Amala and Efo Riro",
+    price: 98,
+    imageUrl:
+      "https://i.pinimg.com/564x/44/8b/3b/448b3bd14e894563172b2fe96da0677e.jpg",
+    itemCount: 4,
+  },
+];
+
+export const currentOrder = [
+  {
+    title: "Amala and Efo Riro",
+    price: 98,
+    imageUrl:
+      "https://i.pinimg.com/564x/44/8b/3b/448b3bd14e894563172b2fe96da0677e.jpg",
+    itemCount: 4,
+  },
+];
