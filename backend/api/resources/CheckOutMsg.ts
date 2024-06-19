ca138116efed8b5f0a288ecbfe82e89963ef479c
justlogicalahmad <rabiu2993@gmail.com>
2024-06-19 09:51:01 -0700

    Create checkout msg

---
+++ backend/api/resources/CheckOutMsg.ts
@@ -0,0 +1,14 @@
+export const CheckOutMsg = {
+  text: "Order placed successfully",
+  sentAt: new Date(),
+  menuOptions: [
+    {
+      title: "to see all options",
+      value: 69,
+    },
+    {
+      title: "to place new order",
+      value: 1,
+    },
+  ],
+};
