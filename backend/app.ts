3d92f477ae87d5d5716760a6001a64016bcba6ed
justlogicalahmad <rabiu2993@gmail.com>
2024-06-20 22:55:08 -0700

    Connect app and socket to db and sessio respectively

--- backend/app.ts
+++ backend/app.ts
@@ -1,8 +1,10 @@
 import { app } from "./socket";
-import cors, { CorsOptions } from "cors";
+import cors from "cors";
 import express from "express";
 import cookieParser from "cookie-parser";
 import morgan from "morgan";
+import mongoose from "mongoose";
+import logger from "./api/utils/logger.util";
 
 interface InternalServerError {
   message: string;
@@ -16,6 +18,14 @@ app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 app.use(morgan("tiny"));
+
+const uri: string = process.env.MONGOURI || "";
+
+// Connects to db
+mongoose
+  .connect(uri)
+  .then(() => console.log("Database connected"))
+  .catch((err) => logger.error("Database connection error"));
 
 // Internal server error
 app.use((err: InternalServerError, req: any, res?: any, next?: any) => {
--- backend/socket.ts
+++ backend/socket.ts
@@ -5,6 +5,10 @@ import { Server } from "socket.io";
 import logger from "../backend/api/utils/logger.util";
 import useGetOptionMessage from "./api/hooks/useGetOptionMessage";
 import { Options } from "./api/resources/Options";
+import { sessionMiddleware } from "./api/middleware/session.middleware";
+import sharedSession from "./api/middleware/session.middleware";
+import { Menu } from "./api/resources/Menu";
+
 dotenv.config();
 
 export const app = express();
@@ -19,18 +23,104 @@ export const io = new Server(server, {
   },
 });
 
-io.on("connection", (socket) => {
-  logger.info(`A user connected - Socket ID: ${socket.id}`);
+// Use session middleware in express
+app.use(sessionMiddleware);
 
-  socket.emit("message", Options);
+io.use(
+  sharedSession(sessionMiddleware, {
+    autoSave: true,
+  })
+);
+
+interface Order {
+  title: string;
+  price: number;
+  imageUrl: string;
+  itemCount: number;
+}
+
+interface OrderLists {
+  currentOrder: Order[];
+  orderHistory: Order[];
+}
+
+interface Orders {
+  [key: string]: OrderLists;
+}
+
+const users: string[] = [];
+const orders: Orders = {};
+
+io.on("connection", (socket) => {
+  const socketId = socket.id;
+  logger.info(`A user connected - Socket ID: ${socketId}`);
+
+  let usersname: string;
+  socket.once("user-joined", (username) => {
+    users.push(username);
+    usersname = username;
+    if (!orders[username]) {
+      orders[username] = {
+        currentOrder: [],
+        orderHistory: [],
+      };
+    }
+
+    if (username) socket.emit("message", Options);
+  });
 
   socket.on("option", (option: number) => {
     const optionMessage = useGetOptionMessage(option);
     socket.emit("optionMessage", optionMessage);
   });
 
+  function countOccurrences(arr: number[]) {
+    const counts: {
+      [key: number]: number;
+    } = {};
+
+    arr.forEach((num) => {
+      counts[num] = (counts[num] || 0) + 1;
+    });
+
+    return counts;
+  }
+
   socket.on("order-items", (orderItems: number[]) => {
-    // return
+    const counts = countOccurrences(orderItems);
+    const menus = Menu.menu;
+
+    Object.keys(counts).forEach((k) => {
+      const key = parseInt(k);
+      const order = {
+        title: menus[key].title,
+        imageUrl: menus[key].imageUrl,
+        itemCount: counts[key],
+        index: key,
+        price: menus[key].price,
+      };
+
+      orders[usersname].currentOrder.push(order);
+    });
+  });
+
+  socket.on("no-order", (orderItems: number[]) => {
+    const noOrder = {
+      text: "No order to place",
+      sentAt: new Date(),
+      menuOptions: [
+        {
+          title: "see all options",
+          value: 69,
+        },
+        {
+          title: "place new order",
+          value: 1,
+        },
+      ],
+      sentBy: 0,
+    };
+    return socket.emit("optionMessage", noOrder);
   });
 
   socket.on("disconnect", () => {
