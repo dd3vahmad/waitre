f7ef798acf9f93c892ac381a9028ee8cae94ad96
justlogicalahmad <rabiu2993@gmail.com>
2024-06-20 23:04:37 -0700

    Fix app component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import Chat from "./components/Chat";
+import NameInput from "./components/NameInput";
 
 const App: React.FC = () => {
-  return <Chat />;
+  const [username, setUsername] = useState<string>("");
+  const botname = "Botswana";
+
+  return (
+    <div className="bg-red-400 flex justify-center items-center w-screen h-screen">
+      {username ? (
+        <Chat username={username} botname={botname} />
+      ) : (
+        <NameInput setUsername={setUsername} />
+      )}
+    </div>
+  );
 };
 
 export default App;
